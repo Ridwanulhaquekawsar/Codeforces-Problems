cd667c42156298db64b74d13161fbf871c6fc9d9
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-10-26 17:01:31 +0600

    Create Prepend_And_Append.cpp

---
+++ Prepend_And_Append.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int t,n,i,j; string s; bool flag;
+
+void solve(){
+    cin>>n>>s;
+    
+    i=0,j=n-1,flag=false;
+    while(s[i]!=s[j]){
+        if(i>j) flag=true;
+        i++,j--;
+    }
+    
+    cout<<(!flag ? j-i+1 : 0)<<'\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false),cin.tie(nullptr);
+    cin>>t; while(t--) solve();
+}
