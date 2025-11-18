b9da29b6a498a70efa4c06c1e624d24a583e6930
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-11-18 08:43:51 +0600

    Create Maximum_GCD.cpp

---
+++ Maximum_GCD.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+ll t,n;
+
+void solve(){
+    cin>>n;
+    
+    if(n % 2) cout<<(n-1)/2;
+    else cout<<n/2;
+}
+
+int main(){
+    FastIO(); cin>>t; while(t--) solve();
+}
