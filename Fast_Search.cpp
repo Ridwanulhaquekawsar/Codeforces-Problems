6636330ea82a744273642a002bf3e5b87ddf7b89
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-12-10 14:47:38 +0600

    Create Fast_Search.cpp

---
+++ Fast_Search.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+ll n, q, l, r;
+vector<ll>v;
+
+void solve(){
+    cin >> n;
+    v.resize(n);
+    
+    for(auto &bx : v) cin >> bx;
+    sort(v.begin(), v.end());
+    
+    cin >> q;
+    
+    while(q--){
+        cin >> l >> r;
+        
+        auto left = lower_bound(v.begin(), v.end(), l);
+        auto right = upper_bound(v.begin(), v.end(), r);
+        
+        cout << (right - left)<< " ";
+    }
+}
+
+int main(){
+    FastIO(); solve();
+}
