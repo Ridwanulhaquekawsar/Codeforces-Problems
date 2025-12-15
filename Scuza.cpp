7b98c50687635bcd7f697fe3d6e3438c131ac849
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-12-15 10:26:03 +0600

    Update Scuza.cpp

--- Scuza.cpp
+++ Scuza.cpp
@@ -19,6 +19,106 @@ using ll = long long int;
 #define s second
 #define pb push_back
 
+#define rs(v, n) (v).resize(n)
+#define as(v, n) (v).assign(n, 0)
+
+#define pll pair<ll, ll>
+#define vll vector<ll>
+
+#define st set<ll>
+#define ml multiset<ll>
+
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define rrep(i, n) for(ll i = n - 1; i >= 0; i--)
+#define rep1(i, a, b) for(ll i = a; i <= b; i++)
+#define rrep1(i, b, a) for(ll i = b; i >= a; i--)
+
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+ll t;
+ll n, q;
+vll prf_max;
+vll prf_sum;
+vll v;
+ll key;
+
+void solve(){
+    in n >> q;
+    rs(v, n);
+    rs(prf_max, n);
+    rs(prf_sum, n);
+    
+    in v[0];
+    prf_sum[0] = v[0];
+    prf_max[0] = v[0];
+    
+    rep1(i, 1, n - 1){
+    
+        in v[i];
+        prf_sum[i] = prf_sum[i - 1] + v[i];
+        prf_max[i] = max(prf_max[i - 1], v[i]);
+    }
+    
+    rep(i, q){
+        
+        in key;
+        
+        auto it = upper_bound(all(prf_max), key);
+        
+        if(it == begin(prf_max)){
+            cout << 0 << " ";
+            continue;
+        }
+        
+        it--;
+        
+        cout << prf_sum[it - prf_max.begin()] << " ";
+    }
+    
+    cout << nl;
+}
+
+int main(){
+    FastIO(); 
+    
+    t = 1;
+    in t;
+    while(t--) solve();
+}
+
+
+
+2nd Approach: 👇
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO() ios::sync_with_stdio(0),cin.tie(0)
+using ll = long long int;
+#define nl '\n'
+
+#define out(x) cout << (x) << nl
+#define in cin >>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define f first
+#define s second
+#define pb push_back
+
 #define pll pair<ll, ll>
 #define vll vector<ll>
 
